b490806fcc30eb31432ddfda54cfd1ada7e94cdc
Giulia <giuliadanso@gmail.com>
2024-04-21 15:01:34 +0200

    initial commit

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,30 +1,238 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tiktok_ui/util/profiletab_1.dart';
+import 'package:tiktok_ui/util/profiletab_2.dart';
+import 'package:tiktok_ui/util/profiletab_3.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          // profile photo
-          Container(
-            height: 120,
-            width: 120,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Empress Giulia',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: const Padding(
+            padding: EdgeInsets.only(right: 12.0),
+            child: Icon(
+              Icons.person_add,
+              color: Colors.black,
+            ),
+          ),
+          actions: const [
+            Icon(
+              Icons.menu,
+              color: Colors.black,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            // profile photo
+            Container(
+              height: 120,
+              width: 120,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey,
+              ),
+            ),
 
-          // username
+            // username
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                '@empressgiulia',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            // number of following,  followers, likes
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                      alignment: Alignment.bottomRight,
+                      child: Column(
+                        children: [
+                          const Text(
+                            '37',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          const Text(
+                            'Following',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.bottomCenter,
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    '8',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Followers',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      alignment: Alignment.bottomLeft,
+                                      child: const Column(
+                                        children: [
+                                          Text(
+                                            '56',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 24),
+                                          ),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            'Likes',
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                      // buttons -> edit profile, insta links, bookmark
 
-          // number of following,  followers, likes
+                      // bio
 
-          // buttons -> edit profile, insta links, bookmark
+                      // default lab controller
+                      ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // buttons -> edit profile, insta link, bookmark
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 40,
+                    ),
+                    child: Text(
+                      'Edit profile',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Icon(
+                        Icons.camera_alt,
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                ),
+                Container(
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Icon(
+                      Icons.bookmark_border,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // bio
 
-          // bio
-
-          // default lab controller
-        ],
+            Text(
+              'bio here',
+              style: TextStyle(
+                color: Colors.grey[700],
+              ),
+            ),
+            // default tab controller
+            const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.grid_3x3, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.favorite, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.lock_outline_rounded, color: Colors.black),
+                ),
+              ],
+            ),
+            const Expanded(
+                child: TabBarView(
+              children: [
+                FirstTab(),
+                SecondTab(),
+                ThirdTab(),
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
