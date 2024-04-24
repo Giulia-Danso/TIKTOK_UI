e46e83c6dc998a06f80352e53e5fdb65bc87ec27
Giulia <giuliadanso@gmail.com>
2024-04-24 22:25:52 +0200

    initial commit

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -15,43 +15,41 @@ class UserProfilePage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Empress Giulia',
-            style: TextStyle(
-              color: Colors.black,
-            ),
+            'Empress Giulia ',
+            style: TextStyle(color: Colors.black),
           ),
           backgroundColor: Colors.transparent,
           elevation: 0,
-          leading: const Padding(
-            padding: EdgeInsets.only(right: 12.0),
-            child: Icon(
-              Icons.person_add,
-              color: Colors.black,
-            ),
+          leading: const Icon(
+            Icons.person_add,
+            color: Colors.black,
           ),
           actions: const [
-            Icon(
-              Icons.menu,
-              color: Colors.black,
-            )
+            Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
           ],
         ),
         backgroundColor: Colors.white,
         body: Column(
           children: [
-            // profile photo
+            //profile photo
             Container(
               height: 120,
               width: 120,
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.grey,
+                color: Colors.grey[200],
               ),
             ),
 
-            // username
+            //username
             const Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(20),
               child: Text(
                 '@empressgiulia',
                 style: TextStyle(
@@ -60,129 +58,139 @@ class UserProfilePage extends StatelessWidget {
                 ),
               ),
             ),
-            // number of following,  followers, likes
+
+            //number of following, followers likes
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
                   child: Container(
-                      alignment: Alignment.centerRight,
-                      child: Column(
-                        children: [
-                          const Text(
-                            '37',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
+                    alignment: Alignment.centerRight,
+                    child: const Column(
+                      children: [
+                        Text(
+                          '37',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
                           ),
-                          const Text(
-                            'Following',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
-                            ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Following',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
                           ),
-                          Expanded(
-                            child: Container(
-                              alignment: Alignment.bottomCenter,
-                              child: Column(
-                                children: [
-                                  const Text(
-                                    '8',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 24,
-                                    ),
-                                  ),
-                                  const Text(
-                                    'Followers',
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      alignment: Alignment.centerLeft,
-                                      child: const Column(
-                                        children: [
-                                          Text(
-                                            '56',
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 24),
-                                          ),
-                                          SizedBox(
-                                            height: 5,
-                                          ),
-                                          Text(
-                                            'Likes',
-                                            style: TextStyle(
-                                              color: Colors.grey,
-                                              fontSize: 15,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Column(
+                      children: [
+                        Text(
+                          '8',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
                           ),
-                        ],
-                      )),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: const Column(
+                      children: [
+                        Text(
+                          '56',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          ' Likes ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
             const SizedBox(
               height: 10,
             ),
-            // buttons -> edit profile, insta link, bookmark
+            //buttons -> edit profile, insta links, bookmark
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(5)),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 15.0,
-                      horizontal: 40,
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: const Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
                     ),
-                    child: Text('Edit profile',
-                        style: TextStyle(color: Colors.black, fontSize: 18)),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 4.0),
                   child: Container(
+                    padding: const EdgeInsets.all(15),
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey.shade300),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Container(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: Colors.grey[800],
-                      ),
+                      border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey[800],
                     ),
                   ),
                 ),
                 Container(
+                  padding: const EdgeInsets.all(15),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(5)),
-                  child: const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Icon(
-                      Icons.bookmark_border,
-                      color: Colors.grey,
-                    ),
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    Icons.bookmark_border,
+                    color: Colors.grey[800],
                   ),
                 ),
               ],
@@ -190,13 +198,10 @@ class UserProfilePage extends StatelessWidget {
             const SizedBox(
               height: 15,
             ),
-            // bio
-
+            //bio
             Text(
               'bio here',
-              style: TextStyle(
-                color: Colors.grey[700],
-              ),
+              style: TextStyle(color: Colors.grey[700]),
             ),
             // default tab controller
             const TabBar(
@@ -208,18 +213,20 @@ class UserProfilePage extends StatelessWidget {
                   icon: Icon(Icons.favorite, color: Colors.black),
                 ),
                 Tab(
-                  icon: Icon(Icons.lock_outline_rounded, color: Colors.black),
+                  icon: Icon(Icons.lock, color: Colors.black),
                 ),
               ],
             ),
+
             const Expanded(
-                child: TabBarView(
-              children: [
-                FirstTab(),
-                SecondTab(),
-                ThirdTab(),
-              ],
-            ))
+              child: TabBarView(
+                children: [
+                  FirstTab(),
+                  SecondTab(),
+                  ThirdTab(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
